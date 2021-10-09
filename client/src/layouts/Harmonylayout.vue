fc910e3a05d56e95f44b602a3e6353dc4ab33c13
Lsickle <davidboy69@hotmail.com>
2021-10-09 12:56:29 -0500

    se añade el codigo restante del index html

--- client/src/layouts/Harmonylayout.vue
+++ client/src/layouts/Harmonylayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="basic-layout">
+    <div class="basic-layout container">
         <Head/>
         <Navbar/>
         <h1>Disfruta de la Armonia<span class="Harmony un concepto de belleza diferente "></span></h1>
@@ -31,10 +31,38 @@
                 text="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
                 textMuted="Last updated 3 mins ago"
             />
+            <div class="row g-0 bg-light position-relative">
+				<div class="col-md-6 mb-md-0 p-md-4">
+					<h5 class="mt-0">Columns with stretched link</h5>
+					<p>
+						Another instance of placeholder content for this other
+						custom component. It is intended to mimic what some
+						real-world content would look like, and we're using it
+						here to give the component a bit of body and size.
+					</p>
+					<a href="#" class="stretched-link">Go somewhere</a>
+				</div>
+				<div class="col-md-6 p-4 ps-md-0">
+					<img src="estilo.jpg" class="w-100" alt="estilo.jpg" />
+				</div>
+			</div>
+			<div class="d-flex position-relative">
+				<img src="ejercicio.jpg" class="w-100" alt="ejercicio.jpg" />
+				<div>
+					<h5 class="mt-0">Custom component with stretched link</h5>
+					<p>
+						This is some placeholder content for the custom
+						component. It is intended to mimic what some real-world
+						content would look like, and we're using it here to give
+						the component a bit of body and size.
+					</p>
+					<a href="#" class="stretched-link">Go somewhere</a>
+				</div>
+			</div>
         </div>
-        <div class="ui container">
+        <!-- <div class="ui container">
             <slot />
-        </div>
+        </div> -->
     </div>
 </template>
 
