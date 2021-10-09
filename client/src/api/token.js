be716ff653ac39932783d6d466221ede7253bbcc
Lsickle <davidboy69@hotmail.com>
2021-10-09 16:20:05 -0500

    gestionando sesion con json webtoken

---
+++ client/src/api/token.js
@@ -0,0 +1,9 @@
+import { TOKEN } from "../utils/constants";
+
+export function setTokenApi(token) {
+  localStorage.setItem(TOKEN, token);
+}
+
+export function getTokenApi() {
+  return localStorage.getItem(TOKEN);
+}
