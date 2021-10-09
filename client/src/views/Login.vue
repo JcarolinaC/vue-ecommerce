825d1c1bf039c489a7f9f3418b598b5826151a51
Lsickle <davidboy69@hotmail.com>
2021-10-09 12:48:47 -0500

    files with capital letters added

---
+++ client/src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+<Basiclayouts>
+    <h1>estamos en login</h1>
+    <router-link to="@/Register">
+    Crear una cuenta 
+    </router-link>
+    </Basiclayouts>
+</template>
+
+<script>
+import Basiclayouts from '@/layouts/Basiclayouts.vue';
+export default {
+    name: 'Login',
+    components: {
+    Basiclayouts,
+  },
+}
+</script>
+
+<style>
+
+</style>
