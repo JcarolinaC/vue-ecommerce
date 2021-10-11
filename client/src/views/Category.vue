fb0cb72c73e67ca3bb4e3a7739a7d5de48574ffb
Lsickle <davidboy69@hotmail.com>
2021-10-11 12:13:07 -0500

    pintando los productos de la categoria

--- client/src/views/Category.vue
+++ client/src/views/Category.vue
@@ -1,46 +1,56 @@
 <template>
 	<Basiclayouts>
 		<h2>estamos en la categoria</h2>
-        {{ products }}
+		<div class="ui grid">
+			<div
+				class="sisxteen wide mobile eight wide tablet four wide computer column"
+				v-for="product in products"
+				:key="product.id"
+			>
+				<Product :product="product" />
+			</div>
+		</div>
 	</Basiclayouts>
 </template>
 
 <script>
-import {ref, onMounted} from 'vue';
-import { useRoute } from 'vue-router';
-import Basiclayouts from '../layouts/Basiclayouts.vue'
-import { getProductsCategory} from '../api/product.js'
-import {getCategoriesApi} from '../api/category.js'
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import Basiclayouts from "../layouts/Basiclayouts.vue";
+import Product from "../components/Product.vue";
+import { getProductsCategory } from "../api/product.js";
+import { getCategoriesApi } from "../api/category.js";
 
 export default {
-    name: 'Category',
+	name: "Category",
 	components: {
-        Basiclayouts
-    },
-    watch:{
-        $router(to, from){
-            this.getProducts(to.params.category)
-        },
-    },
-    setup() {
-        let products = ref(null);
-        const {params} = useRoute();
+		Basiclayouts,
+		Product,
+	},
+	watch: {
+		$router(to, from) {
+			this.getProducts(to.params.category);
+		},
+	},
+	setup() {
+		let products = ref(null);
+		const { params } = useRoute();
 
-        onMounted( () => {
-            getProducts(params.Category);
-        });
+		onMounted(() => {
+			getProducts(params.Category);
+		});
 
-        const getProducts = async (category) => {
-            const response = await getProductsCategory(category);
-            products.value = response;
-            console.log(response);
-        };
+		const getProducts = async (category) => {
+			const response = await getProductsCategory(category);
+			products.value = response;
+			console.log(response);
+		};
 
-        return {
-            getProducts,
-            products
-        };
-    },
+		return {
+			getProducts,
+			products,
+		};
+	},
 };
 </script>
 
