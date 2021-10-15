318e76f61fb945c59c56dfe6fba44b18179970ea
Lsickle <davidboy69@hotmail.com>
2021-10-15 12:01:10 -0500

    ajustada la transparencia del logo

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,14 +3,12 @@
     <h2> Harmony un estilo de vida diferente </h2>
     <!-- <h1>Ultimos productos</h1> -->
     <div class="ui grid">
-      <div 
-		 class="sisxteen wide mobile eight wide tablet  four wide computer  column"
-		 v-for="product in products"
-		 :key="product.id"
-      >
-		<Product :product="product" />
+      <div class="sisxteen wide mobile eight wide tablet  four wide computer  column" v-for="product in products" :key="product.id">
+        <Product :product="product" />
       </div>
     </div>
+    
+    
 
   </Basiclayouts>
 </template>
@@ -22,11 +20,21 @@ import { getProducts } from "@/api/product.js";
 import Product from "@/components/Product.vue";
 // @ is an alias to /src
 
+import Navbar from '@/components/Navbar.vue'
+import Head from '@/components/Head.vue'
+import Carousel from '@/components/Carousel.vue'
+import Card from '@/components/Card.vue'
+
+
 export default {
   name: 'Home',
     components: {
       Basiclayouts,
-      Product
+      Product,
+      Navbar,
+        Head,
+        Carousel,
+        Card,
     },
     setup(){
       let products = ref(null);
